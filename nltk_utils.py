20250bf935b6d023e733fc7051dfd5db303c8e5d
Diya Ghosh <diya1692005ne@gmail.com>
2025-08-21 20:37:59 +0530

    Update nltk_utils.py

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,34 +1,40 @@
-# Installing Libraries
+# nltk_utils.py
 import numpy as np
-import pandas as pd
 import nltk
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
-nltk.download('punkt')
 
-stemmer=PorterStemmer()
+# Download required NLTK resources safely
+nltk.download('punkt', quiet=True)  # only 'punkt' is needed
 
-# Tokenize Sentences (breaking sentence into words)
+# Initialize stemmer
+stemmer = PorterStemmer()
+
+# Tokenize sentences (breaking sentence into words)
 def tokenize(sentence):
     return word_tokenize(sentence)
 
-# Stemming (converting into root words)
-def stemming(words):
-    return stemmer.stem(words.lower())
+# Stemming (converting word to its root form)
+def stemming(word):
+    return stemmer.stem(word.lower())
 
-# Bag of words return array of 0 (if corresponding word of words array not present in tokenized sentence) or 1(if present) in their corresponding position
-def bag_of_words(tokenized_sentences,words):
-    bag=np.zeros(len(words),dtype=np.float32)
-    tokenized_words=[]
-    stemmed_words=[]
-    for w in tokenized_sentences:
-        tokenized_words.append(stemming(w))
-    for w in words:
-        stemmed_words.append(stemming(w))
-    for index,w in enumerate(stemmed_words):
+# Bag of words
+# Returns an array where each position is 0 or 1
+# 1 if corresponding word in 'words' exists in 'tokenized_sentence'
+def bag_of_words(tokenized_sentence, words):
+    bag = np.zeros(len(words), dtype=np.float32)
+
+    # Stem each word in the sentence
+    tokenized_words = [stemming(w) for w in tokenized_sentence]
+
+    # Stem the reference words
+    stemmed_words = [stemming(w) for w in words]
+
+    # Set bag[i] = 1 if word exists in sentence
+    for idx, w in enumerate(stemmed_words):
         if w in tokenized_words:
-            bag[index]=1
-            
-    return bag        
+            bag[idx] = 1
+
+    return bag
 
 
